f7a6b3206dbf5db6d7607b3b4c0235d3f7b9b6fe
arifucoder <arifucoder@gmail.com>
2025-06-27 15:59:34 +0600

    login form added

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -4,18 +4,18 @@ import { Link, Outlet } from "react-router";
 import authImg from "../assets/authImage.png";
 const AuthLayout = () => {
 	return (
-		<div>
-			<div>
+		<div className="flex h-screen">
+			<div className="w-3/5 px-11 py-14 h-full">
 				<div>
-					<Link to="/">
+					<Link to="/" className="flex gap-0 items-end">
 						<Logo></Logo>
 					</Link>
 				</div>
-				<div>
+				<div className="mx-auto flex items-center justify-center h-full">
 					<Outlet></Outlet>
 				</div>
 			</div>
-			<div>
+			<div className="w-2/5 h-full bg-[#FAFDF0] flex items-center justify-center">
 				<img src={authImg} alt="Authentication system Image" />
 			</div>
 		</div>
--- src/pages/auth/Login/Login.jsx
+++ src/pages/auth/Login/Login.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<div className="w-[384px]">
+			<h2 className="text-5xl font-extrabold mb-1">Welcome Back</h2>
+			<p className="mb-5">Login with Profast</p>
+			<form className="fieldset">
+				<label className="label">Email</label>
+				<input type="email" className="input mb-3 w-full" placeholder="Email" />
+				<label className="label">Password</label>
+				<input type="password" className="input w-full" placeholder="Password" />
+				<div>
+					<a className="link link-hover">Forgot password?</a>
+				</div>
+				<button className="btn mt-2 bg-ccaeb66 text-black">Login</button>
+			</form>
+			<p className="mt-1 text-gray-400">
+				Don’t have any account? <Link>Register</Link>
+			</p>
+		</div>
+	);
 };
 
 export default Login;
